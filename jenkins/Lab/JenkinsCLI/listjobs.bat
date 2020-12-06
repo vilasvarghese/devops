844bd9f49a8cb6eb5616cfebdae26c2cae4150ed
vilasvarghese <vilas.varghese@gmail.com>
2020-12-06 23:13:45 +0530

    add presentation and other fixes

--- jenkins/Lab/JenkinsCLI/listjobs.bat
+++ jenkins/Lab/JenkinsCLI/listjobs.bat
@@ -1,6 +1,6 @@
 set JENKINS_URL=http://localhost:8080
 set JENKINS_USER=vilas
-set JENKINS_TOKEN=1130a7a11d937ae6b9ef6e4572aeb12198 #can be pwd or token
+set JENKINS_TOKEN=11463a5a4d76fabea12dd6312f3e48cea3 #can be pwd or token
 
 echo "Display all the jobs in Jenkins: "
-java -jar jenkins-cli.jar -s %JENKINS_URL% -auth vilas:1130a7a11d937ae6b9ef6e4572aeb12198 list-jobs
+java -jar C:\Program Files\Jenkins\jenkins-cli.jar -s %JENKINS_URL% -auth vilas:11463a5a4d76fabea12dd6312f3e48cea3 list-jobs
